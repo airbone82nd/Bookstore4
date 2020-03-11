a56c9f1d3af913b944db57e3889d98256273c07a
airbone82nd <61398002+airbone82nd@users.noreply.github.com>
2020-03-11 20:27:09 +0700

    Save sension DEBugV.2

--- resources/views/Books/index.blade.php
+++ resources/views/Books/index.blade.php
@@ -2,65 +2,39 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-lg-10 col-lg-offset-1">
-
-            <?=link_to('books/create',$title='เพิ่มข้อมูล',['class'=>'btn btn-primary'],$secure=null);?>
-
-                <div class="card-header h3">
-
-                  รายการหนังสือในระบบ [ ทั้งหมด{{$books->total()}} เล่ม]
-
-                </div>
-                <div class="card-body">
-                    <table class="table table-striped">
-                        <tr>
-                            <th> รหัส</th>
-                            <th> ชื่อหนังสือ</th>
-                            <th>ราคา</th>
-                            <th>หมวดหนังสือ</th>
-                            <th>รูปภาพหนังสือ</th>
-                            <th>แก้ไข</th>
-                            <th>ลบ</th>
-                        </tr>
-                        @foreach ($books as $book)
-                        <tr>
-                            <td>{{$book->id}} </td>
-                            <td>{{$book->title}} </td>
-                            <td>{{number_format($book->price,2)}} </td>
-                            <td>{{$book->typebooks->name}} </td>
-                            <td>
-
-                                <a href="{{asset('images/'.$book->image) }}" data-lity><img src="{{asset('images/resize/'.$book->image)}}" style="width:50px"></a>
-                            </td>
-                            <td>
-                            <a href="{{ url('/books/'.$book->id.'/edit')}}">แก้ไข</a>
-                            </td>
-                            <td>
-                            <?=Form::open(array('url'=>'books/'. $book->id,'method' =>  'delete','onsubmit'=>'return confirm("แน่ใจว่าต้องการลบข้อมูล?");')) ?>
-                            <button type="submit" class="btn btn-danger">ลบ</button>
-                            {!! Form::close() !!}
-                        </tr>
-
-                        @endforeach
-                    </table>
-                    <br>
-                    {!!$books->render()!!}
-            </div>
-        </div>
+        <div class="col-md col-md-offset-2">
+<div class=" card-header h3 ">
+    เพิ่มข้อมูลรายการหนังสือ
+</div>
+<div class=" card-body">
+    {!! Form::open(array('url'=>'books','files'=>true)) !!}
+    <div class="Form-group">
+        <?=Form::label('title','ชื่อหนังสือ'); ?>
+        <?= Form::text('title',null,['class'=>'form-control','placeholder'=>'ชื่อหนังสือ']); ?>
     </div>
+    <div class="Form-group">
+    <?=Form::label('price','ราคา'); ?>
+        <?= Form::text('price',null,['class'=>'form-control','placeholder'=>'ราคาหนังสือ']); ?>
+    </div>
+    <div class=" form-group">
+        {!!Form::label('typebooks_id','ประเภทหนังสือ');!!}
+        <?=Form::select('typebooks_id',
+        App\typebooks::all()->pluck('name','id'),null,['class'=>'form-control','placeholder'=>'เลือกประเภทหนังสือ']);?>
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('image','รูปภาพ');!!}
+        <?=Form::file('image',null,['class'=>'form-control'])?>
+    </div>
+    <div class="form-group">
+        <?=Form::submit('บันทึก',['class'=>'btn btn-primary']);?>
+    </div>
+    {!!Form::close()!!}
+
 </div>
-</div>
-@endsection
-@section('footer')
-@if (session()->has('status'))
-        <script>
-            swal({
-                title:"<?php echo session()->get('status'); ?>",
-                text: "",
-                timer:2000,
-                type:'success',
-                showConfirmButton: false
-});
-    </script>
-@endif
+    </div>
+
+    </div>
+
+
 @endsection
